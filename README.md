1f8c76d8663d0d8c536fb447087e93df95b21f24
notewio <fzheng413@gmail.com>
2022-02-05 16:16:59 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # actuator
 Touchscreen gestures for Linux
+
+## Usage
+actuator looks for a config file at `$XDG_CONFIG_HOME/actuator/Actuator.toml`. It must contain the following values:
+
+* `device_path = string` Path to touchscreen device
+* `edge_tolerance = integer` How far away from the edge of the screen counts as the edge
+* `min_distance = integer` How far you have to swipe to count as a gesture and not a tap
+* `screen_height & screen_width = integer` Height and width of screen, find with evtest.
+
+## Actions
+Actions are defined in the `[actions]` section of the config file, as `action_name = command`. Available actions are:
+
+* 1\_from\_top
+* 1\_from\_bottom
+* 1\_from\_left
+* 1\_from\_right
+* 2\_pinch
+* 2\_spread
+* 2\_up
+* 2\_down
+* 2\_left
+* 2\_right
---
+++ src/main.rs
@@ -0,0 +1,172 @@
+use directories::BaseDirs;
+use evdev::{Device, EventType};
+use std::{collections::HashMap, fs};
+use toml::Value;
+
+struct Finger {
+    x: Vec<i32>,
+    y: Vec<i32>,
+}
+impl Finger {
+    fn new() -> Self {
+        return Finger {
+            x: vec![],
+            y: vec![],
+        };
+    }
+    fn empty(&self) -> bool {
+        self.x.len() == 0 || self.y.len() == 0
+    }
+    fn start(&self) -> (i32, i32) {
+        (self.x[0], self.y[0])
+    }
+    fn end(&self) -> (i32, i32) {
+        (self.x[self.x.len() - 1], self.y[self.y.len() - 1])
+    }
+    fn delta(&self) -> (i32, i32) {
+        (
+            self.x[self.x.len() - 1] - self.x[0],
+            self.y[self.y.len() - 1] - self.y[0],
+        )
+    }
+}
+
+macro_rules! run_action {
+    ($a: expr, $k: expr) => {
+        println!($k);
+        let a = $a.get($k);
+        if let Some(v) = a {
+            run_script::spawn_script!(v).unwrap();
+        } else {
+            println!("No action found");
+        }
+    };
+}
+
+fn main() {
+    // Load configuration
+    let base_dirs = BaseDirs::new().expect("Could not find configuration file folder");
+    let config_path = base_dirs
+        .config_dir()
+        .join("actuator/Actuator.toml")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    let config = fs::read_to_string(config_path)
+        .expect("Could not read config file")
+        .parse::<Value>()
+        .unwrap();
+    let device_path = config["device"].as_str().unwrap();
+    let edge_tolerance = config["edge_tolerance"].as_integer().unwrap() as i32;
+    let min_distance = config["min_distance"].as_integer().unwrap() as i32;
+    let screen_height = config["screen_height"].as_integer().unwrap() as i32;
+    let screen_width = config["screen_width"].as_integer().unwrap() as i32;
+
+    let mut actions = HashMap::new();
+    if let Value::Table(t) = &config["actions"] {
+        for (key, value) in t {
+            actions.insert(key.as_str(), value.as_str().unwrap());
+        }
+    }
+
+    let mut input_device = Device::open(device_path).unwrap();
+    let mut current_finger = 0;
+    let mut held_fingers = 0;
+    let mut fingers = vec![];
+    loop {
+        for ev in input_device.fetch_events().unwrap() {
+            if ev.code() == 57 && ev.value() == -1 {
+                held_fingers -= 1;
+            } else if ev.code() == 57 && ev.value() >= 0 {
+                held_fingers += 1;
+            } else if ev.code() == 47 {
+                current_finger = ev.value() as usize;
+            }
+
+            if current_finger >= fingers.len() {
+                fingers.push(Finger::new());
+            }
+
+            if ev.event_type() == EventType::ABSOLUTE {
+                match ev.code() {
+                    53 => fingers[current_finger].x.push(ev.value()),
+                    54 => fingers[current_finger].y.push(ev.value()),
+                    _ => {}
+                }
+            }
+        }
+
+        if held_fingers == 0 {
+            match fingers.len() {
+                1 => {
+                    let f = &fingers[0];
+                    if !f.empty() {
+                        let (sx, sy) = f.start();
+                        let (dx, dy) = f.delta();
+                        if dx.abs() + dy.abs() < min_distance {
+                        } else if dy.abs() > dx.abs() {
+                            if sy < edge_tolerance {
+                                run_action!(actions, "1_from_top");
+                            } else if sy > screen_height - edge_tolerance {
+                                run_action!(actions, "1_from_bottom");
+                            }
+                        } else {
+                            if sx < edge_tolerance {
+                                run_action!(actions, "1_from_left");
+                            }
+                            if sx > screen_width - edge_tolerance {
+                                run_action!(actions, "1_from_right");
+                            }
+                        }
+                    }
+                }
+                2 => {
+                    let f0 = &fingers[0];
+                    let f1 = &fingers[1];
+                    if !(f0.empty() || f1.empty()) {
+                        let (dx0, dy0) = f0.delta();
+                        let (dx1, dy1) = f1.delta();
+
+                        if dx0.abs() + dy0.abs() < min_distance
+                            || dx1.abs() + dy1.abs() < min_distance
+                        {
+                        } else if dx0 * dx1 + dy0 * dy1 < 0 {
+                            let (sx0, sy0) = f0.start();
+                            let (sx1, sy1) = f1.start();
+                            let sdist = (sx1 - sx0).pow(2) + (sy1 - sy0).pow(2);
+                            let (ex0, ey0) = f0.end();
+                            let (ex1, ey1) = f1.end();
+                            let edist = (ex1 - ex0).pow(2) + (ey1 - ey0).pow(2);
+                            if edist < sdist {
+                                run_action!(actions, "2_pinch");
+                            } else {
+                                run_action!(actions, "2_spread");
+                            }
+                        } else {
+                            // finger 0 takes priority. is it worth calculating average here?...
+                            if dy0.abs() > dx0.abs() {
+                                if dy0 > 0 {
+                                    run_action!(actions, "2_down");
+                                } else {
+                                    run_action!(actions, "2_up");
+                                }
+                            } else {
+                                if dx0 > 0 {
+                                    run_action!(actions, "2_right");
+                                } else {
+                                    run_action!(actions, "2_left");
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            fingers.clear();
+            held_fingers = 0;
+            current_finger = 0;
+        }
+    }
+}
